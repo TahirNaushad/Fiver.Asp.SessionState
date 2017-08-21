dbe9ca9e24d4685666df410fd5748a9bb5a4ad0e
Tahir Naushasd <t.naushad25@gmail.com>
2017-08-21 12:36:17 +0100

    asp.net core 2 session state

--- Fiver.Asp.SessionState/Startup.cs
+++ Fiver.Asp.SessionState/Startup.cs
@@ -11,23 +11,62 @@ namespace Fiver.Asp.SessionState
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        //public void ConfigureServices(
+        //    IServiceCollection services)
+        //{
+        //    services.AddDistributedMemoryCache();
+        //    services.AddSession();
+        //}
+
+        public void ConfigureServices(
+           IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Name = ".Fiver.Session";
+                //options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //public void Configure(
+        //    IApplicationBuilder app, 
+        //    IHostingEnvironment env)
+        //{
+        //    app.UseSession();
+
+        //    app.Use(async (context, next) =>
+        //    {
+        //        context.Session.SetString("GreetingMessage", "Hello Session State");
+        //        await next();
+        //    });
+
+        //    app.Run(async (context) =>
+        //    {
+        //        var message = context.Session.GetString("GreetingMessage");
+        //        await context.Response.WriteAsync($"{message}");
+        //    });
+        //}
+
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseSession();
+
+            app.Use(async (context, next) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                context.Session.SetObject("CurrentUser",
+                    new UserInfo { Username = "James", Email = "james@bond.com" });
+                await next();
+            });
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var user = context.Session.GetObject<UserInfo>("CurrentUser");
+                await context.Response.WriteAsync($"{user.Username}, {user.Email}");
             });
         }
     }
---
+++ Fiver.Asp.SessionState/UserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Fiver.Asp.SessionState
+{
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
